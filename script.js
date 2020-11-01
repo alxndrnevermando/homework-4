6301fb50d4afca019edad1f79a7ad0b3f9e63e20
Armando Contreras <armandocontreras62596@gmail.com>
2020-10-31 19:51:27 -0700

    finished adding my questions

--- script.js
+++ script.js
@@ -9,23 +9,31 @@ function startQuiz() {
     start.classList.add('hide');
     //Makes question and buttons reappear
     questionElement.classList.remove("hide");
-    
-
-
-}
+    setNextQuestion();
+};
 
 function setNextQuestion() {
 
-}
+};
 
 function selectAnswer() {
 
-}
+};
 
 var questions = [
     {
         question: "1. What does HTML stand for?",
         choices: ["HyperText Markup Logic", "HyperTalk Makeup Language", "HiperText Markup Language", "HyperText Markup Language"],
         correctAnswer: 3
+    },
+    {
+        question: "2. What year was Javascript created?",
+        choices: ["1993", "1994", "1995", "1996"],
+        correctAnswer: 2
+    },
+    {
+        question: "3. Term used to describe a function inside of an object?",
+        choices: ["Method", "Parameter", "Event", "Function"],
+        correctAnswer: 0
     },
 ]
